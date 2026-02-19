d1e14cfeaaf7740e378b905307bdd55c264daa56
Anoop George <anoopgeorge418@gmail.com>
2026-02-19 19:32:37 +0530

    Root page design and navbar page designs

---
+++ user/components/AppIcon.tsx
@@ -0,0 +1,8 @@
+export const AppIcon = () => {
+	return (
+		<div className='bg-purple-500 w-10 h-10 rounded-[10px] flex justify-center items-center'>
+			{/* Top icon shape */}
+			<div className='bg-white w-5 h-5 rounded-[5px]'></div>
+		</div>
+	);
+};
